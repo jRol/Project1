a413f4bbfed23b02bb8a04a88dd03ad3bb50edb5
LatoyaBogle <concordiae20@gmail.com>
2018-09-21 23:58:51 -0400

    changed css and html

--- assets/javascript/fav.js
+++ assets/javascript/fav.js
@@ -9,28 +9,41 @@ $(document).ready(function() {
         storageBucket: "gwbootcamp-97ba0.appspot.com",
         messagingSenderId: "454079581913"
       };
-    firebase.initializeApp(config);
 
-    var database = firebase.database();
+     
+      firebase.initializeApp(config);
 
-    database.ref('/meetupFavs').on("child_added", function(snapshot) {
-        var sv = snapshot.val();
-
-        
-        console.log(sv.favMeetup.name);
-        //dont change this
-        var listings = $('.container');
-        
-
-       
-
-        
-        var details = $("<div>");
-        details.html(sv.favMeetup.name);
-
-        listings.append(details);
-
-    });
-    
-});
-
+      var database = firebase.database();
+      var email;
+      firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            console.log("user signed in");
+            console.log(user.displayName);
+            email=user.email;
+            console.log(email);
+            database.ref('/meetupFavs').orderByChild('email').equalTo(email).on("child_added", function(snapshot) {
+              var sv = snapshot.val();
+              console.log(sv);
+              
+              console.log(sv.favMeetup.name);
+              //dont change this
+              var listings = $('.container');
+              
+      
+             
+      
+              
+              var details = $("<div>");
+              details.html("<p>"+sv.favMeetup.name+"</p>");
+      
+              listings.append(details);
+      
+          });
+          } else {
+            console.log("boo hoo. no user");
+          }
+      });
+      
+      
+      
+  });
--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -68,3 +68,39 @@ function getCurrentLocation (position) {
     });
     
 }
+function signIn() {
+    var config = {
+        apiKey: "AIzaSyC9pE2ORuZUcAnZM_4fnUDSScgurVLBbN8",
+        authDomain: "gwbootcamp-97ba0.firebaseapp.com",
+        databaseURL: "https://gwbootcamp-97ba0.firebaseio.com",
+        projectId: "gwbootcamp-97ba0",
+        storageBucket: "gwbootcamp-97ba0.appspot.com",
+        messagingSenderId: "454079581913"
+        };
+    firebase.initializeApp(config);
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log("Sign In successful");
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        token = result.credential.accessToken;
+        // console.log(token);
+        if(longitude && latitude) {
+            // Store all content into sessionStorage
+            sessionStorage.setItem("longitude", longitude);
+            sessionStorage.setItem("latitude", latitude);
+            window.location.href = "main.html";
+            
+
+        }
+        }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        });
+}
