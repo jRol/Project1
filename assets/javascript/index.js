4b47d65461f6a84a494d4bd72eda0dacf47d0fdc
aishtiaq <38296709+aishtiaq@users.noreply.github.com>
2018-09-25 21:05:37 -0400

    Added comments

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,7 @@
 var latitude, longitude,token="false";
 
 $(document).ready(function() {
+    //initialize firebase
     var config = {
         apiKey: "AIzaSyC9pE2ORuZUcAnZM_4fnUDSScgurVLBbN8",
         authDomain: "gwbootcamp-97ba0.firebaseapp.com",
@@ -12,27 +13,15 @@ $(document).ready(function() {
 
     firebase.initializeApp(config);
     sessionStorage.clear();
+    //call geolocation
     geoLoc();
-       
+    
+    //button for Logging in
     $("#confirm").on("click", function(){
-        console.log("random change");
-        // if(token === "false") {
-        //     console.log("signing in");
-        //     signIn();
-        // } else {
-        //     console.log("here i am");
-        //     alert("hello");
-        //     if(longitude && latitude) {
-        //         // Store all content into sessionStorage
-        //         sessionStorage.setItem("longitude", longitude);
-        //         sessionStorage.setItem("latitude", latitude);
-        //         window.location.href = "main.html";
-                
- 
-        //     }
-        // }
+        //check if user is already signed in        
         firebase.auth().onAuthStateChanged(function(user) {
-            console.log("in auth");
+            //console.log("in auth");
+            //if they are signed in and we have long and lat log in and direct to main.html
             if (user) {
                 if(longitude && latitude) {
                     // Store all content into sessionStorage
@@ -41,7 +30,8 @@ $(document).ready(function() {
                     window.location.href = "main.html";
                 }
             } else {
-                console.log("signing in");
+                //console.log("signing in");
+                //if not logged in, sign in.
                 signIn();
             }
         });
@@ -50,21 +40,22 @@ $(document).ready(function() {
 });
 
 function geoLoc() {
-  
+    //if geo location is enabled get the values
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getCurrentLocation);
        
     } else {
+        //if it is disabled, nothing will happen
         console.log ("geolocation not available");
     }
 }
 
 function getCurrentLocation (position) {
- 
+    //get longitude and latitude
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
     console.log ("lon is "+longitude+" and lat is "+latitude);
-    
+    //make an openweather call to get location name
     var APIKey = "9cf07e60efb34da49a4496096daf288b";
     var queryURL="https://api.openweathermap.org/data/2.5/weather?lat="+latitude + "&lon=" + longitude + "&APPID=" + APIKey;
 
@@ -77,6 +68,7 @@ function getCurrentLocation (position) {
     
             
     });    
+    //envoke mapbox to show the geocoder.
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWlzaHRpYXEiLCJhIjoiY2psdnBtY2VvMDUyMTNxcXN0ZGJwcjd2YiJ9.jiV57t9pdOYOb8iJc_xABg';
     
     var mapL = new mapboxgl.Map({
@@ -92,9 +84,9 @@ function getCurrentLocation (position) {
     geocoder.on('result', function(ev) {
        
         
-        //once the location changes run displayMeetupAPI to get new data 
-        longitude=ev.result.geometry.coordinates[0];
-        latitude=ev.result.geometry.coordinates[1];
+    //once the location changes run displayMeetupAPI to get new data 
+    longitude=ev.result.geometry.coordinates[0];
+    latitude=ev.result.geometry.coordinates[1];
         
     });
     
