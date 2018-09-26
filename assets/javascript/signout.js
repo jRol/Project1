80eb447a8b9e0e33e14b2b596bea45963e6c10e8
aishtiaq <38296709+aishtiaq@users.noreply.github.com>
2018-09-25 21:11:26 -0400

    added comments

--- assets/javascript/signout.js
+++ assets/javascript/signout.js
@@ -1,7 +1,8 @@
 
+//This function is called when Sign Out is clicked
 
 $(document).on("click","#signout", function(){
-    console.log("button clicked");
+    //console.log("button clicked");
     firebase.auth().signOut().then(function() {
         console.log("signed out");
         //window.location.href = "index.html";
@@ -10,7 +11,7 @@ $(document).on("click","#signout", function(){
         // An error happened.
         console.log("error");
       });
-    
+      //send the user back to main page
       window.location.href = "index.html";
 });
 
